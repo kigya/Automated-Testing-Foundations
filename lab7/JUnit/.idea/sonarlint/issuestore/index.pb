9de87c482169d9b92b43394d780737b520f1734b
kirillborichevskiy <kirilborichevskiy@gmail.com>
2022-11-17 13:10:56 +0300

    Delete unused files

--- lab7/JUnit/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,5 +0,0 @@
-
-@
-build.gradle.kts,d/b/dbcff70658daf80b53ce624f6adcaa529df5ed8d
-X
-(src/test/kotlin/assertions/Assertions.kt,e/3/e35b7ace5802c37c3a54215f5cba45de02027538
--- lab7/TestsJUnit/TestsJUnit/src/main/kotlin/Person.kt
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate
-
-data class Person(
-    val firstName: String = "",
-    val lastName: String? = "",
-    val gender: Gender = Gender.F,
-    val birthday: LocalDate = LocalDate.now()
-)
