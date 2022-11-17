8a506836707c360325bb4465fc07bfbb86dda293
kirillborichevskiy <kirilborichevskiy@gmail.com>
2022-11-17 13:08:26 +0300

    Add tests

---
+++ lab7/Optional/src/main/kotlin/Person.kt
@@ -0,0 +1,8 @@
+import java.time.LocalDate
+
+data class Person(
+    val firstName: String = "",
+    val lastName: String? = "",
+    val gender: Gender = Gender.F,
+    val birthday: LocalDate = LocalDate.now()
+)
